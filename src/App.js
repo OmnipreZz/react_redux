1df9b8b256b554e8ab23360bf1571089608a445e
Omni <geoffrey.frioli@gmail.com>
2020-09-08 09:29:51 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import { TodoList } from './components/TodoList';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  );
+}
+
+export default App;
